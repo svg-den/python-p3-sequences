e1eaa7c4be63ddf2afbe4983c4c2b1feaaa2ccf0
svg-den <sundaywanjiru@gmail.com>
2024-02-27 19:33:42 +0300

    Done assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
+# lib/sequences.py
+
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0, 1]
+
+    while len(fibonacci_sequence) < length:
+        number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(number)
+
+    print(fibonacci_sequence[:length])
+
+# Run pytest -x to check your work
+
+pass
